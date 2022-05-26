78a6df1e28d894385151a200dd1a46dcb712dc5a
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-26 09:19:43 +0300

    added registration model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Registration(models.Model):
+    useremail = models.CharField(max_length=300)
+    username = models.EmailField(max_length=300)
+    passwords = models.CharField(max_length=300)
+    datedadded = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'Registration'
+
+    def __str__(self):
+        return self.username
